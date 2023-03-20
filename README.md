cdc91f0037ba57266dbe7ea94e96f970f37da024
LILIANA VELAZQUEZ <lilivelazquezz@gmail.com>
2023-03-20 11:22:44 -0700

    Take Home Code Challenge Two

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
  ======
  
  [Main List](https://lilivelazquezz.github.io/TakeHomeCodeChallenge/ "All Take Home Code Challenges")
+
  [One](https://lilivelazquezz.github.io/TakeHomeCodeChallenge/One/ "Take Home Code Challenge One")
+
+ [Two](https://lilivelazquezz.github.io/TakeHomeCodeChallenge/Two/ "Take Home Code Challenge Two")
  
---
+++ Two/script.js
@@ -0,0 +1,76 @@
+let form = document.forms["my-form"];
+form.addEventListener("submit", getValues);
+
+let dropdownDay = document.getElementById("selectDay");
+for( let i = 1; i < 32; i++) {
+    dropdownDay[dropdownDay.length] = new Option(i , i);
+}
+
+let selectMonths = new Array("January", "February", "March", "April", "May","June", "July", "August", "September", "October","November", "December");
+let dropdownMonth = document.getElementById("selectMonth");
+for (var i = 0; i < selectMonths.length; ++i) {
+    // Append the element to the end of Array list
+    dropdownMonth[dropdownMonth.length] = new Option(selectMonths[i], selectMonths[i]);
+}
+
+let dropdownYear = document.getElementById("selectYear");
+for( let i = 2023; i > 1923; i--) {
+    dropdownYear[dropdownYear.length] = new Option(i , i);
+}
+
+function getValues(event){
+	event.preventDefault();
+
+	let data = {
+		"day": this['day'].value,
+        "month": this['month'].value,
+        "year": this['year'].value,
+	}
+
+	let	out = `
+		<p>Your birthday: <span>${data['day']}</span> <span>${data['month']}</span> <span>${data['year']}</span></p>
+	`;
+
+	document.querySelector(".out code").innerHTML = out;
+	console.log(data['day']);
+    console.log(data['month']);
+    console.log(data['year']);
+
+    const countTo = data['day'] + " " + data['month'] + " " + 2024;
+
+console.log(countTo);
+const c = setInterval(() => {
+    const endDate = new Date(countTo);
+   // console.log(endDate + " this is end date")
+    const currentDate = new Date()
+   // console.log(currentDate)
+    const totalSeconds = (endDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600/24) - 365;
+    const hours = Math.floor(totalSeconds / 3600)  % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    const countDown = document.getElementById("countdown");
+    countDown.textContent =`${days}days ${hours}hrs : ${minutes}min : ${seconds}s` + " for your next b-day";
+    if(totalSeconds < 0 ){
+        clearInterval(c);
+        countDown.textContent = "Happy Birth Day!!!";
+    }
+})
+
+} // end of form input
+
+// Color Generator
+/*
+let color = Math.random();
+color = Math.random().toString(16);
+color = Math.random().toString(16).substring(2, 8); //.substring(x, y)
+console.log(color);
+*/
+const hex = document.querySelector(".hex");
+const btn = document.querySelector(".generate");
+const generateColor = () => {
+    color = Math.random().toString(16).substring(2, 8); 
+    document.body.style.backgroundColor = "#" + color;
+    hex.innerHTML = "Today's Random Hex Color: " + "#" + color;
+};
+form.addEventListener("submit", generateColor);
